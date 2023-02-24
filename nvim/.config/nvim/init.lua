5bde1edc9237528a2a3d956b981c5af11e383716
Russell Shurts <rshurts@users.noreply.github.com>
2023-02-23 19:26:44 -0600

    Tweak for deno

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,3 +5,8 @@ require("config.lazy")
 -- disable netrw at the start for nvim-tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- appropriately highlight codefences returned from denols
+vim.g.markdown_fenced_languages = {
+  "ts=typescript",
+}
