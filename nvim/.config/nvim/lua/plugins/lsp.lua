1c6d9d8ea6fce6a985e7777e5d0f6266a44b9b6c
Russell Shurts <rshurts@users.noreply.github.com>
2023-03-14 13:16:26 -0500

    Add astro

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -13,6 +13,7 @@ return {
 
       require("mason-lspconfig").setup({
         ensure_installed = {
+          "astro",
           "denols",
           "gopls",
           "lua_ls",
@@ -87,6 +88,10 @@ return {
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+      lspconfig["astro"].setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
       lspconfig["denols"].setup({
         capabilities = capabilities,
         on_attach = on_attach,
