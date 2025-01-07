ef6fd9168861da7743eab18fd54def256d7b59af
Russell Shurts <rshurts@users.noreply.github.com>
2025-01-06 22:44:30 -0600

    Add mini utilities

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -219,4 +219,14 @@ return {
       })
     end,
   },
+
+  -- utilies
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      require("mini.ai").setup({ n_lines = 500 })
+      require("mini.pairs").setup()
+      require("mini.surround").setup()
+    end,
+  },
 }
