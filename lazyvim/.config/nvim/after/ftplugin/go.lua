6428df222d1c4a56a2d10396f7b1343e1ecfcfa8
Russell Shurts <rshurts@users.noreply.github.com>
2026-03-26 11:18:24 -0500

    use 4 space tabs

--- lazyvim/.config/nvim/after/ftplugin/go.lua
+++ lazyvim/.config/nvim/after/ftplugin/go.lua
@@ -1,6 +1,6 @@
 local set = vim.opt_local
 
 set.expandtab = false -- use hard tabs
-set.shiftwidth = 8 -- use 8 column indents
-set.softtabstop = 8 -- insert mode tab and backspace use 8 columns
-set.tabspot = 8 -- actual tabs are 8 columns
+set.shiftwidth = 4 -- use 4 column indents
+set.softtabstop = 4 -- insert mode tab and backspace use 4 columns
+set.tabspot = 4 -- actual tabs are 4 columns
